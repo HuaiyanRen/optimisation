38b92cbbe57f949ba812642cc33ec95c79fc50df
HuaiyanRen <928003585@qq.com>
2024-04-11 11:01:14 +1000

    Create merge_csv.py

---
+++ merge_csv.py
@@ -0,0 +1,27 @@
+import os
+import csv
+
+folder_path = r'C:\Users\u7151703\Desktop\research\optimisation\data\f81_qmix\c3t50r0'
+
+new_csv_path = os.path.join(folder_path,'sum_result.csv')
+
+
+with open(new_csv_path, mode='w', newline='') as new_csv_file:
+    writer = csv.writer(new_csv_file)
+
+    writer.writerow(['method', 'class', 'lnl', 'bic', 'lrt', 'df', 'time', 'rep']) 
+
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.csv'):
+            file_path = os.path.join(folder_path, filename)
+            repname = filename.split('.csv')[0]
+
+            with open(file_path, mode='r', newline='') as csv_file:
+                reader = csv.reader(csv_file)
+                next(reader)  
+
+                for row in reader:
+                    writer.writerow(row + [repname])
+
+
