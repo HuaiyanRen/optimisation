16d36b419c8b7269b680071e4d5d70586e210dfe
HuaiyanRen <928003585@qq.com>
2024-04-29 16:24:57 +1000

    Update merge_csv.py
    Fix a bug

--- merge_csv.py
+++ merge_csv.py
@@ -1,9 +1,16 @@
 import os
 import csv
 
-folder_path = r'C:\Users\u7151703\Desktop\research\optimisation\data\f81_qmix\c6t25r25k'
+folder_path = r'C:\Users\u7151703\Desktop\research\optimisation\data\f81_qmix\c6t50r05k'
 
 new_csv_path = os.path.join(folder_path,'sum_result.csv')
+
+
+csv_list = []
+
+for filename in os.listdir(folder_path):
+    if filename.endswith('.csv'):
+        csv_list.append(filename)
 
 
 with open(new_csv_path, mode='w', newline='') as new_csv_file:
@@ -12,16 +19,15 @@ with open(new_csv_path, mode='w', newline='') as new_csv_file:
     writer.writerow(['method', 'class', 'lnl', 'bic', 'lrt', 'df', 'time', 'rep']) 
 
 
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.csv'):
-            file_path = os.path.join(folder_path, filename)
-            repname = filename.split('.csv')[0]
+    for filename in csv_list:
+        file_path = os.path.join(folder_path, filename)
+        repname = filename.split('.csv')[0]
 
-            with open(file_path, mode='r', newline='') as csv_file:
-                reader = csv.reader(csv_file)
-                next(reader)  
+        with open(file_path, mode='r', newline='') as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)  
 
-                for row in reader:
-                    writer.writerow(row + [repname])
+            for row in reader:
+                writer.writerow(row + [repname])
 
 
